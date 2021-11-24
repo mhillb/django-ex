0dc945579eb6fd5f77a1eada8bc3252e4d42f0ba
mikael.hillborg@ibm.com <mikael.hillborg@ibm.com>
2021-11-24 20:41:20 +0100

    Misc.

--- welcome/views.py
+++ welcome/views.py
@@ -4,20 +4,13 @@ from django.conf import settings
 from django.http import HttpResponse
 
 from . import database
-from .models import PageView
+#from .models import PageView
 
 # Create your views here.
 
 def index(request):
     """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
-    return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
+    return render(request, 'welcome/index.html')
 
 def retrieve(request):
     html = "<html><head><title></title></head><body>Testing ... ABC abc 123...</body></html>"
@@ -25,4 +18,4 @@ def retrieve(request):
 
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
-    return HttpResponse(PageView.objects.count())
+    return HttpResponse("")
